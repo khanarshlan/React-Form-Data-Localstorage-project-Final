ba9f65e268ddbdb28a73849eea8deba54bb9dda3
khanarshlan <khanarshlan1999@gmail.com>
2023-04-17 03:14:22 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white text-center p-4 mt-3">
+      <h2>Design And Developed By : Arshlan</h2>
+    </div>
+  );
+};
+
+export default Footer;
